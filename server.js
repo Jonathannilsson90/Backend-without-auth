ee0fae2b75c8a27f5c6d25a2dd25c19cf73de341
Shehryar Hassan <phonetech195@gmail.com>
2022-12-01 01:32:57 +0100

    Börjat med att göra API.

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-Test commit!
+const express = require('express');
+const app = express();
+const items = require("./routes/items");
+
+var bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
