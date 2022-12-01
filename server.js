aaf89cac71778dd97508017df7d6b61dc63ce2bc
Shehryar Hassan <phonetech195@gmail.com>
2022-12-01 01:33:36 +0100

    Rättat filsökväg

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const items = require("./routes/items");
+const items = require("./routes/routes");
 
 var bodyParser = require('body-parser');
 
