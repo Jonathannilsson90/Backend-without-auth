6247c4618e52f3e28a0924bfccfaa113b79bd2e1
Shehryar Hassan <phonetech195@gmail.com>
2022-12-01 21:24:12 +0100

    import crud, initialize router

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,11 @@
-// routes here
+import express from "express";
+import {
+  createUser,
+  getUser,
+  getID,
+  deleteUser,
+  updateUser,
+} from "../controllers/users.js";
+
+//initialize router
+const router = express.Router();
