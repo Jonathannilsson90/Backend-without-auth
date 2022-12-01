a882fbe9be50b2aa8cae5ac7ef876b6c4505167c
Shehryar Hassan <phonetech195@gmail.com>
2022-12-01 21:29:16 +0100

    uuvid import, två test users

--- controllers/users.js
+++ controllers/users.js
@@ -1,1 +1,15 @@
-/// controllers here
+//export all the callback functions for the CRUD operations
+import { v4 as uuidv4 } from "uuid";
+
+let users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 27,
+  },
+];
