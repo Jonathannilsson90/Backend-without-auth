309abab6371c1e124ee4abd138b7c4f2eff5bd0d
Shehryar Hassan <phonetech195@gmail.com>
2022-12-01 02:00:25 +0100

    Råkat göra totalt fel!!! ska påbörja på nytt.

--- server.js
+++ server.js
@@ -1,7 +1,1 @@
-const express = require('express');
-const app = express();
-const items = require("./routes/routes");
-
-var bodyParser = require('body-parser');
-
-const PORT = process.env.PORT || 3000;
+Test commit!
