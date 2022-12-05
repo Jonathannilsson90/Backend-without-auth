5b90a93936367db300f039b71a40c7e78c9073ea
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2022-12-05 14:13:06 +0100

    fixed-routes

--- routes/clothingRoutes.js
+++ routes/clothingRoutes.js
@@ -1,16 +1,18 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const {
-  getClothes,
-  getClothesById,
-  postClothing,
-  updateClothing,
-  removeClothing,
-} = require("../controllers/clothingControllers");
+    getClothes,
+    getClothesById,
+    postClothing,
+    updateClothing,
+    removeClothing,
+  } = require("../controllers/clothingControllers");
 
-router.route("/").get(getClothes).post(postClothing);
+router.route("/").get(getClothes).post(postClothing)
 
 // WARNING: Prettier will format this line of code wrong. It needs to be concatinated to work.
-router.get("/:id").get(getClothesById).put(updateClothing).delete(removeClothing);
+router.route("/:id").get(getClothesById).put(updateClothing).delete(removeClothing)
+
 
 module.exports = router;
+
