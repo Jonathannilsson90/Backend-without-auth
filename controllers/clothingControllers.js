36f23e282882f34ddcf089e4f1373ad4f2aa9987
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2022-12-05 14:12:42 +0100

    Fixed-my-own-mistake

--- controllers/clothingControllers.js
+++ controllers/clothingControllers.js
@@ -2,12 +2,12 @@
 import { v4 as uuidv4 } from "uuid";
 */
 
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const clothes = [
-  { type: "shirt", color: "red", size: "M" },
+  /*  { type: "shirt", color: "red", size: "M" },
   { type: "jeans", color: "blue", size: "L" },
-  { type: "socks", color: "white", size: "S" },
+  { type: "socks", color: "white", size: "S" }, */
 ];
 
 //          Get clothes
@@ -16,31 +16,30 @@ const getClothes = (req, res) => {
   res.status(200).json({
     data: clothes,
     sucess: "success",
-    message: "Clothing found"
-  }) 
-}
+    message: "Clothing found",
+  });
+};
 
 //          Get clothes
 // ROUTE    GET /api/clothes/:id
 const getClothesById = (req, res) => {
   res.status(200).json({
     sucess: "success",
-    message: "Found clothing"
-  })
-}
-
+    message: "Found clothing",
+  });
+};
 
 //          POST clothes
 // ROUTE    POST /api/clothes/:id
 const postClothing = (req, res) => {
   const cloth = req.body;
-  clothes.push({  ...cloth, id: uuidv4()})
+  clothes.push({ ...cloth, id: uuidv4() });
   res.send({
     sucess: "success",
-    message:"New clothing added",
-  })
-  
-}
+    message: "New clothing added",
+  });
+};
+
 //          PUT clothes
 // ROUTE    PUT /api/clothes/:id
 
@@ -55,49 +54,34 @@ const updateClothing = (req, res) => {
     size,
   };
 
-  const clothingIndex = clothes.findIndex((u) => (u.id == clothingId));
+  const clothingIndex = clothes.findIndex((u) => u.id == clothingId);
   clothes[clothingIndex] = newClothing;
 
   req.status(200).json({
     status: "success",
-    msg: "Clothing updated"
+    msg: "Clothing updated",
   });
 };
-
-/*const updateClothing = (req, res) => {
-  const { id } = req.params;
-  const { type, color, size } = req.body;
-  const clothingToBeUpdated = clothes.find((cloth) => cloth.id === id);
-  if (type) clothingToBeUpdated.type = type;
-  if (color) clothingToBeUpdated.color = color;
-  if (size) clothingToBeUpdated.size = size;
-  res.send({
-    sucess: true,
-    message:"Clothing updated succesfully",
-  })
-};
-*/
 
 //          Delete
 // ROUTE    Delete /api/clothes/:id
 
-const removeClothing = (req,res) => {
-  const clothingId = req.params.id;
+const removeClothing = (req, res) => {
+  const { clothingId } = req.params.id;
 
   const clothingIndex = clothes.findIndex((u) => u.id == clothingId);
   clothes.splice(clothingIndex, 1);
 
   res.status(200).json({
-    status: "success", 
-    msg: "Deleted Clothing"
-  })
-}
-
+    status: "success",
+    msg: "Deleted Clothing",
+  });
+};
 
 module.exports = {
   getClothes,
-getClothesById,
-postClothing,
-updateClothing,
-removeClothing
-}
+  getClothesById,
+  postClothing,
+  updateClothing,
+  removeClothing,
+};
